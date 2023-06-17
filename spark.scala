a01f4cce80f2c9c32e8e01b7c0bf91cabd37b472
queukat <75810528+queukat@users.noreply.github.com>
2023-06-18 00:02:07 +0300

    Update spark.scala

--- spark.scala
+++ spark.scala
@@ -1,52 +1,100 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import java.sql.DriverManager
+import org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils
 
 object HiveToGreenplum {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("HiveToGreenplum").getOrCreate()
     val hiveTableName = args(0)
     val greenplumTableName = args(1)
-    val greenplumSchema = args(2)
-    val greenplumURL = args(3)
-    val greenplumUser = args(4)
-    val greenplumPassword = args(5)
+    val greenplumURL = args(2)
+    val greenplumUser = args(3)
+    val greenplumPassword = args(4)
+    val useExternalTable = args(5).toBoolean // true - use external table, false - direct write
+
+    // JDBC URL for Greenplum
+    val greenplumJdbcUrl = s"jdbc:postgresql://$greenplumURL?user=$greenplumUser&password=$greenplumPassword"
     
-    // Read the Hive table in ORC format
-    val hiveTableDF = spark.read.format("orc").load(hiveTableName)
-    
-    // Cast the data types
-    val castedDF = hiveTableDF.selectExpr(
-      "cast(boolean_column as bool) as boolean_column",
-      "cast(int_column as int4) as int_column",
-      "cast(smallint_column as int2) as smallint_column",
-      "cast(tinyint_column as int2) as tinyint_column",
-      "cast(bigint_column as int8) as bigint_column",
-      "cast(float_column as float4) as float_column",
-      "cast(double_column as float8) as double_column",
-      "cast(string_column as text) as string_column",
-      "cast(binary_column as bytea) as binary_column",
-      "cast(timestamp_column as timestamp) as timestamp_column"
-    )
-    
-    // Create the Greenplum external table
-    castedDF.write.format("io.pivotal.greenplum.spark.GreenplumDataSource")
-      .option("url", greenplumURL)
-      .option("user", greenplumUser)
-      .option("password", greenplumPassword)
-      .option("dbschema", greenplumSchema)
-      .option("table", greenplumTableName)
-      .mode("overwrite")
-      .save()
-      
-    // Upload the table to Greenplum
-    castedDF.write.format("io.pivotal.greenplum.spark.GreenplumRelationProvider")
-      .option("url", greenplumURL)
-      .option("user", greenplumUser)
-      .option("password", greenplumPassword)
-      .option("dbschema", greenplumSchema)
-      .option("table", greenplumTableName)
-      .mode("overwrite")
-      .save()
-      
-    spark.stop()
+    try {
+      // Read the Hive table
+      val hiveTableDF = spark.read.table(hiveTableName)
+
+      // Automatically detect and cast the data types
+      val castedDF = hiveTableDF.select(hiveTableDF.columns.map { colName =>
+        val colType = hiveTableDF.schema(colName).dataType
+        colType match {
+          case _: ByteType      => hiveTableDF(colName).cast(IntegerType).as(colName)
+          case _: ShortType     => hiveTableDF(colName).cast(IntegerType).as(colName)
+          case _: BinaryType    => hiveTableDF(colName).cast(StringType).as(colName)
+          case _                => hiveTableDF(colName)
+        }
+      }: _*)
+
+      // Create table schema in Greenplum
+      val hiveTableSchema = castedDF.schema
+      val createTableSQL = JdbcUtils.schemaString(hiveTableSchema, "postgresql", greenplumTableName)
+
+      val conn = DriverManager.getConnection(greenplumJdbcUrl)
+      try {
+        val stmt = conn.createStatement()
+        stmt.execute(createTableSQL)
+      } catch {
+        case e: Exception =>
+          println(s"Error while creating table schema in Greenplum: ${e.getMessage}")
+          e.printStackTrace()
+      } finally {
+        conn.close()
+      }
+
+      if (useExternalTable) {
+        // Create the Greenplum external table
+        castedDF.write.format("io.pivotal.greenplum.spark.GreenplumDataSource")
+          .option("url", greenplumURL)
+          .option("user", greenplumUser)
+          .option("password", greenplumPassword)
+          .option("table", greenplumTableName)
+          .mode("overwrite")
+          .save()
+
+        println(s"Successfully created external table $greenplumTableName")
+      } else {
+        // Write data directly to Greenplum
+        castedDF.repartition(20).write // repartition data into 20 partitions for parallel writing
+          .format("io.pivotal.greenplum.spark.GreenplumRelationProvider")
+          .option("url", greenplumURL)
+          .option("user", greenplumUser)
+          .option("password", greenplumPassword)
+          .option("table", greenplumTableName)
+          .mode("overwrite")
+          .save()
+
+        println(s"Successfully wrote data to $greenplumTableName")
+      }
+
+      // Data integrity check
+      val greenplumCount = spark.read.format("io.pivotal.greenplum.spark.GreenplumRelationProvider")
+        .option("url", greenplumURL)
+        .option("user", greenplumUser)
+        .option("password", greenplumPassword)
+        .option("table", greenplumTableName)
+        .load()
+        .count()
+
+      val hiveCount = hiveTableDF.count()
+
+      if (greenplumCount == hiveCount) {
+        println("Data integrity check passed")
+      } else {
+        println("Data integrity check failed")
+      }
+
+    } catch {
+      case e: Exception =>
+        println(s"An error occurred during the migration: ${e.getMessage}")
+        e.printStackTrace()
+    } finally {
+      spark.stop()
+    }
   }
 }
